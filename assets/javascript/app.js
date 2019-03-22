f38069bb70fe21efa30546e20f6a81b901e0eb1e
Sarah B <smblair87@gmail.com>
2019-03-21 20:44:38 -0700

    Added variables and objects for questions, options, and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,61 @@
-// Trivia Questions
+//Global variables
 
-// 1. Which restaurant chain was Pam banned from?
-// 2. Who is Michael Scott's favorite actress?
-// 3. What is printed on Michael's coffee mug? 
-// 4. Who does Angela name her son after? 
-// 5. What is the name of the security guard? 
-// 6. Who's car is hit with a watermelon during safety training? 
-// 7. What does Jim put for Dwight's middle name on his ID badge? 
-// 8. Who has two thumbs and hates Todd Packer? 
-// 9. Who is the main character in Threat Level Midnight?
-// 10. What did Dwight dress up as in the first Halloween episode? 
+// Game Objects
 
+// Trivia questions
+var questions = {
+    question1: "Which restaurant chain was Pam banned from?",
+    question2: "Who is Michael Scott's favorite actress?",
+    question3: "What is printed on Michael's coffee mug?",
+    question4: "Who does Angela name her son after?",
+    question5: "What is the name of the security guard?",
+    question6: "Who's car is hit with a watermelon during safety training?",
+    question7: "What does Jim put for Dwight's middle name on his ID badge?",
+    question8: "Who has two thumbs and hates Todd Packer?",
+    question9: "Who is the main character in Threat Level Midnight?",
+    question10: "What did Dwight dress up as in the first Halloween episode?"
+}
 
-// Answer Key
+// Array of options for user to guess from
+var options = {
+    question1: ["Denny's", "Chili's", "Applebees", "IHOP"],
+    question2: ["Meryl Streep", "Julia Roberts", "Nicole Kidman", "Anne Hathaway"],
+    question3: ["World's Most Amazing Boss", "World's Greatest Boss", "World's Worst Boss", "World's Best Boss"],
+    question4: ["Her father", "Her cat", "Her best friend", "A celebrity"],
+    question5: ["Frank", "Bob", "Hank", "Greg"],
+    question6: ["Stanley", "Creed", "Jim", "Meredith"],
+    question7: ["Butts", "Fart", "Danger", "Wait for it"],
+    question8: ["Pam", "Michael Scott", "Phyllis", "Jim"],
+    question9: ["Michael Scarn", "Nicolas Cage", "Michael Scorpio", "Michael Buble"],
+    question10: ["Beet farmer", "A Sith Lord", "Jim", "Spock"],
+}
 
-// 1. Chili's
-// 2. Meryl Streep
-// 3. World's Best Boss
-// 4. Her cat
-// 5. Hank
-// 6. Stanley
-// 7. Fart
-// 8. Jim
-// 9. Michael Scarn
-// 10. A Sith Lord
+// Answer key
+var answers = {
+    question1: ["Chili's"],
+    question2: ["Meryl Streep"],
+    question3: ["World's Best Boss"],
+    question4: ["Her cat"],
+    question5: ["Hank"],
+    question6: ["Stanley"],
+    question7: ["Fart"],
+    question8: ["Jim"],
+    question9: ["Michael Scarn"],
+    question10: ["A Sith Lord"],
+}
 
+var correctGuesses = 0;
+var incorrectGuesses = 0;
+var unanswered = 0;
+var timer = // 30 seconds
 
 // Game pseudocode
 
 // User opens page
+// Makes function available after page loads
+$(document).ready(function(){
+
+
 
 // User clicks on start button to begin game
 
