9a6bf372e57d78cc4957ba9faad6ff61edbab5b0
Sarah B <smblair87@gmail.com>
2019-03-21 20:16:20 -0700

    Added folders, files, images, and wrote some psuedocode to get started

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+// Trivia Questions
+
+// 1. Which restaurant chain was Pam banned from?
+// 2. Who is Michael Scott's favorite actress?
+// 3. What is printed on Michael's coffee mug? 
+// 4. Who does Angela name her son after? 
+// 5. What is the name of the security guard? 
+// 6. Who's car is hit with a watermelon during safety training? 
+// 7. What does Jim put for Dwight's middle name on his ID badge? 
+// 8. Who has two thumbs and hates Todd Packer? 
+// 9. Who is the main character in Threat Level Midnight?
+// 10. What did Dwight dress up as in the first Halloween episode? 
+
+
+// Answer Key
+
+// 1. Chili's
+// 2. Meryl Streep
+// 3. World's Best Boss
+// 4. Her cat
+// 5. Hank
+// 6. Stanley
+// 7. Fart
+// 8. Jim
+// 9. Michael Scarn
+// 10. A Sith Lord
+
+
+// Game pseudocode
+
+// User opens page
+
+// User clicks on start button to begin game
+
+/* User sees first question and 4 possible answers, timer starts counting down from 
+30 seconds  */
+
+// If user guesses correctly, show "correct!" and image for 5 seconds
+// If user guess incorrectly, show "incorrect!," correct answer, and image for 5 seconds
+// If user does not guess, show correct answer for 5 seconds
+
+// After 5 seconds, user is taken to second question, repeat process 
+
+/* After user has gone through all 10 questions, they are taken to a summary that shows
+correct guesses, incorrect guesses, and unanswered questions, along with a start over button 
+to reset the entire game */
+
+
